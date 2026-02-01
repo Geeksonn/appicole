8ea0a7608e8d9bbc428bfbb92ccc7b2a83976d0a
Olivier Sputael <sputael.o@gmail.com>
2026-02-01 13:42:12 +0100

    design OK, TODO rating & fav icon

--- src/components/menu/beer-item.tsx
+++ src/components/menu/beer-item.tsx
@@ -20,15 +20,13 @@ const BeerItem: React.FunctionComponent<Props> = (props) => {
     const imgUrl = `${process.env.EXPO_PUBLIC_IMG_CDN}/${image_card_path}`;
 
     return (
-        <View className='flex flex-row justify-around h-36 mx-3 my-8 bg-white rounded-xl drop-shadow-[0_0_30px_rgba(0,0,0,0.05)]'>
-            <View className='w-1/3 -mt-4'>
-                <Image source={imgUrl} contentFit='cover' transition={1000} style={{ flex: 1 }} />
+        <View className='flex flex-row justify-around h-36 mx-3 my-6 bg-white rounded-xl ring-2 ring-gray-300/10 shadow-lg'>
+            <View className='w-1/3 -mt-7'>
+                <Image source={imgUrl} contentFit='contain' transition={1000} style={{ flex: 1 }} />
             </View>
-            <View className='flex flex-col w-2/3 py-2'>
-                <View className='flex justify-between items-center pr-2'>
-                    <Text className='font-title font-bold text-brass-green text-xl'>{name}</Text>
-                </View>
-                <Text className='text-brass-grey text-sm'>{`${type} - ${degree_integer},${degree_decimal} %`}</Text>
+            <View className='flex w-2/3 py-2'>
+                <Text className='font-title font-bold text-brass-green text-xl'>{name}</Text>
+                <Text className='text-gray-400 text-sm'>{`${type} - ${degree_integer},${degree_decimal} %`}</Text>
                 <View className='flex justify-between mr-4 items-center mt-4'>
                     {/* <BeerRating rating={rating} numberOfVotes={numberOfVotes} /> */}
                     {showCaveIcon && (
